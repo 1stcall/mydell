eae397ac02be17621d9153345ae0b8a49f02c49e
Carl McAlwane <carlmcalwane@hotmail.co.uk>
2023-03-18 19:35:18 +0000

    removed .bash_aliases heredoc. need a rethink

--- install_apts_dell.sh
+++ install_apts_dell.sh
@@ -355,42 +355,6 @@ mount --verbose --all
 #
 # Installing login scripts (~/.bashrc & .bash_aliases)
 #
-log "Replacing ~/.bash_aliases"
-rm -vf /home/$userName/.bash_aliases
-
-cat << EOF > /home/$userName/.bash_aliases
-alias ll='ls -lAhtr --group-directories-first'
-alias histgrep='history | grep'
-alias sudo='sudo '
-alias apt='sudo $(which apt)'
-alias reboot='sudo $(which reboot)'
-alias halt='sudo $(which alias)'
-alias poweroff='sudo $(which poweroff)'
-alias grep='grep --color'
-alias higrep='grep --color=always -e "^" -e '
-#alias trashboot='sudo /home/$userName/.local/usr/bin/trashboot.sh'
-alias iptables='sudo $(which iptables)'
-
-function negrep(){
-#	set -x
-	files="${@:--}"
-	for fileName in $files
-	do 
-		printf "\n    #### fileName=%s ####\n\n" ${fileName}
-		grep --color=always -n -e "^" -e "^E:" -e "^W:" "${fileName}" \
-			| grep --color=always -i \
-			"error\|warning\|invalid\|problem\|fail\|fatal\|panic\|not found\|missing\|could not\|unrecognized\|unable\|unavailable\|doh\|not specified\|omitting\|cannot\|No such file\|unknown" 2>&1
-		:
-	done
-#	set +x
-}
-export -f negrep
-
-function qqbc(){
-	echo "scale=${2:-2}; $1" | bc -l
-}
-export -f qqbc
-EOF
 #
 # Create .gitconfig
 #
